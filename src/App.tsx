6a899d9a16b88c57f9997316ceb010911ee1e0ef
MingkaiChen <44886949+MingkaiChen@users.noreply.github.com>
2023-09-12 23:35:01 -0400

    Suppress no-unused-vars error.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,17 @@ function App() {
 
   const [geoPackage, setGeoPackage] = useState<GeoPackage>();
 
+  /**
+   * THIS FUNCTION IS ONLY USE TO SUPPRESS THE TS ERROR.
+   * PLEASE REMOVE THIS FUNCTION WHEN ALL VARS ARE USED.
+   */
+  function handleUnusedVars() {
+    if (geoPackage) {
+      console.log(geoPackage);
+    }
+  }
+  handleUnusedVars();
+
   return (
     <FileLoaderView setGeoPackage={(geoPackage: GeoPackage) => setGeoPackage(geoPackage)}>
     </FileLoaderView>
