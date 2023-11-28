6d33b1618fb3db49fd6a66d5ac87197fc71b5369
NotakingButaGod <214561763whe@gmail.com>
2023-11-28 01:30:55 -0500

    rename (unfinished)

--- src/Views/Components/PopupPage/UserProfile.tsx
+++ src/Views/Components/PopupPage/UserProfile.tsx
@@ -1,20 +1,63 @@
 import { useState, useEffect } from "react";
 import SearchBar from "../Elements/SearchBar";
 import IUserProfileProps from "./Interfaces/IUserProfileProps";
-import {HiEye, HiEyeOff, HiPencil, HiCheck, HiX } from "react-icons/hi";
+import { HiEye, HiEyeOff, HiPencil, HiCheck, HiX } from "react-icons/hi";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import "./css/userProfile.css";
 
+interface IUserResponse {
+  user_id: string;
+  user_name: string;
+  role: string;
+}
+
 const UserProfile = (userProfile: IUserProfileProps) => {
-  const { user, logout} = useAuth0();
+  const { user, logout } = useAuth0();
   const [items, setItems] = useState(userProfile.projectList);
   const [isEditing, setIsEditing] = useState(false); // New state for editing mode
-  const [newUsername, setNewUsername] = useState<string | undefined>(user?.nickname);
+  const [newUsername, setNewUsername] = useState<string | undefined>(
+    user?.nickname
+  );
+  const [userData, setUserData] = useState<IUserResponse | null>(null);
 
   useEffect(() => {
     setNewUsername(user?.nickname);
     console.log(user);
+    // Fetch user data when the component mounts and the user object is available
+
+    const fetchUserData = async (email: string) => {
+      try {
+        const response = await fetch(
+          `https://zaunmap-6b1455b08c9b.herokuapp.com/api/user?user_id=${encodeURIComponent(
+            email
+          )}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              // Include any necessary authentication headers
+              Authorization: `Bearer ${user?.token}`, // Replace with actual token from Auth0 if available
+            },
+          }
+        );
+
+        if (response.ok) {
+          const userData: IUserResponse = await response.json();
+          console.log("User data retrieved successfully:", userData);
+          setUserData(userData);
+        } else {
+          console.error("Failed to retrieve user data");
+          // Handle errors
+        }
+      } catch (error) {
+        console.error("Error while fetching user data", error);
+        // Handle errors
+      }
+    };
+    if (user?.email) {
+      fetchUserData(user.email);
+    }
   }, [user]);
 
   const setItemsPublic = (id: number) => {
@@ -26,24 +69,44 @@ const UserProfile = (userProfile: IUserProfileProps) => {
   };
 
   // Function to handle username update
-  const updateUsername = async () => {
+  const updateUsername = async (
+    email: string | undefined,
+    new_name: string | undefined
+  ) => {
+    if (!email || !new_name) {
+      console.error("Email or new username is undefined");
+      return;
+    }
+    if (!userData || !newUsername) {
+      console.error("Missing user data or new username");
+      return;
+    }
+
     try {
-      // Replace with your actual backend API endpoint
-      const response = await fetch('/api/updateUsername', {
-        method: 'PUT', // Changed to PUT
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ newUsername }),
-      });
+      const response = await fetch(
+        `https://zaunmap-6b1455b08c9b.herokuapp.com/api/user/rename?user_id=${encodeURIComponent(
+          email
+        )}&new_name=${encodeURIComponent(new_name)}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user?.token}`, // Make sure you use the correct token
+          },
+          body: JSON.stringify({
+            user_id: email,
+            new_name: new_name,
+          }),
+        }
+      );
 
       if (response.ok) {
         console.log("Username updated successfully");
-        console.log(user);
-        // Update the UI as necessary
+        setUserData({ ...userData, user_name: new_name });
+        setNewUsername(new_name);
       } else {
         console.error("Failed to update username");
-        // Handle errors
+        // Handle errors, possibly by parsing the response JSON
       }
     } catch (error) {
       console.error("Error while updating username", error);
@@ -64,13 +127,19 @@ const UserProfile = (userProfile: IUserProfileProps) => {
               onChange={(e) => setNewUsername(e.target.value)}
               className="edit-username-input"
             />
-            <HiCheck onClick={updateUsername} className="icon-check"/>
-            <HiX onClick={() => setIsEditing(false)} className="icon-close"/>
+            <HiCheck
+              onClick={() => updateUsername(user?.email, newUsername)}
+              className="icon-check"
+            />
+            <HiX onClick={() => setIsEditing(false)} className="icon-close" />
           </div>
         ) : (
           <div className="display-username">
             <p className="username-text">{newUsername}</p>
-            <HiPencil onClick={() => setIsEditing(true)} className="icon-edit"/>
+            <HiPencil
+              onClick={() => setIsEditing(true)}
+              className="icon-edit"
+            />
           </div>
         )}
       </div>
