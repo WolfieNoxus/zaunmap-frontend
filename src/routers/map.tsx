0a3e821cfe1d39ebc70d480600cb8d3fd96fcada
MingkaiChen <44886949+MingkaiChen@users.noreply.github.com>
2023-11-28 17:57:34 -0500

    Add testcases.

--- src/routers/map.tsx
+++ src/routers/map.tsx
@@ -1,24 +1,22 @@
-import React, { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { MapWrapper } from "../Views/MapWrapper";
-import IMap from '../Interfaces/IMap';
-import apiClient from '../services/apiClient';
-import GeoJSONMapView from '../Views/GeoJSONMapView';
+// import IMap from '../Interfaces/IMap';
+// import apiClient from '../services/apiClient';
 
-async function getFileFromUrl(url: string, filename: string): Promise<File> {
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
+// async function getFileFromUrl(url: string, filename: string): Promise<File> {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//         throw new Error(`HTTP error! status: ${response.status}`);
+//     }
 
-    const blob = await response.blob();
+//     const blob = await response.blob();
 
-    const file = new File([blob], filename, {
-        type: blob.type,
-    });
+//     const file = new File([blob], filename, {
+//         type: blob.type,
+//     });
 
-    return file;
-}
+//     return file;
+// }
 
 function Map() {
 
@@ -26,73 +24,73 @@ function Map() {
     if (typeof mapId !== "string") {
         throw new Error("Map ID is not a string");
     }
-    const [loading, setLoading] = useState(true);
+    // const [loading, setLoading] = useState(true);
 
-    const [ map, setMap ] = useState<IMap>({
-        _id: "",
-        name: "",
-        tags: [],
-        author: "",
-        public: false,
-        object_id: "",
-        createdAt: "",
-        updatedAt: "",
-        likes: 0,
-        dislikes: 0,
-        description: "",
-    });
+    // const [ map, setMap ] = useState<IMap>({
+    //     _id: "",
+    //     name: "",
+    //     tags: [],
+    //     author: "",
+    //     public: false,
+    //     object_id: "",
+    //     createdAt: "",
+    //     updatedAt: "",
+    //     likes: 0,
+    //     dislikes: 0,
+    //     description: "",
+    // });
 
-    const [geojson, setGeojson] = useState<GeoJSON.FeatureCollection<GeoJSON.GeometryObject>>({
-        type: "FeatureCollection",
-        features: [],
-    });
+    // const [geojson, setGeojson] = useState<GeoJSON.FeatureCollection<GeoJSON.GeometryObject>>({
+    //     type: "FeatureCollection",
+    //     features: [],
+    // });
 
-    useEffect(() => {
-        const fetchMapMeta = async (sub: string) => {
-          try {
-            const response = await apiClient.get(`/map?_id=${mapId}`);
-            if (response.status === 200) {
-              const userData: IMap = response.data;
-              // console.log("User data retrieved successfully:", userData);
-              setMap(userData);
-            } else {
-              console.error("Failed to retrieve map data");
-              // Handle errors
-            }
-          } catch (err) {
-            console.error("Error while fetching map data", err);
-          } finally {
-            setLoading(false);
-          }
-        };
+    // useEffect(() => {
+    //     const fetchMapMeta = async (sub: string) => {
+    //       try {
+    //         const response = await apiClient.get(`/map?_id=${mapId}`);
+    //         if (response.status === 200) {
+    //           const userData: IMap = response.data;
+    //           // console.log("User data retrieved successfully:", userData);
+    //           setMap(userData);
+    //         } else {
+    //           console.error("Failed to retrieve map data");
+    //           // Handle errors
+    //         }
+    //       } catch (err) {
+    //         console.error("Error while fetching map data", err);
+    //       } finally {
+    //         setLoading(false);
+    //       }
+    //     };
         
-        fetchMapMeta(mapId);
-      }, []);
+    //     fetchMapMeta(mapId);
+    //   }, []);
 
-      useEffect(() => {
-        const fetchMapData = async (sub: string) => {
-          try {
-            const response = await fetch(`http://zaunmap.pages.dev/${map.author}/${map.object_id}`);
-            if (response.status === 200) {
-              const geoJson: GeoJSON.FeatureCollection<GeoJSON.GeometryObject> = await response.json();
-              setGeojson(geoJson);
-            } else {
-              console.error("Failed to retrieve map data");
-            }
+    //   useEffect(() => {
+    //     const fetchMapData = async (sub: string) => {
+    //       try {
+    //         const response = await fetch(`http://zaunmap.pages.dev/${map.author}/${map.object_id}`);
+    //         if (response.status === 200) {
+    //           const geoJson: GeoJSON.FeatureCollection<GeoJSON.GeometryObject> = await response.json();
+    //           setGeojson(geoJson);
+    //         } else {
+    //           console.error("Failed to retrieve map data");
+    //         }
 
-          } catch (err) {
-            console.error("Error while fetching map data", err);
-          } finally {
-            setLoading(false);
-          }
-        };
+    //       } catch (err) {
+    //         console.error("Error while fetching map data", err);
+    //       } finally {
+    //         setLoading(false);
+    //       }
+    //     };
         
-        fetchMapData(mapId);
-      }, [map]);
+    //     fetchMapData(mapId);
+    //   }, [map]);
     
     return (
         <div>
-            Unimplemented Map View. Map ID: {mapId}.
+            Unimplemented Map View. Map ID: {mapId}
         </div>
     );
 };
