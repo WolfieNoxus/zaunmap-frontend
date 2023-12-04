742c02743f5321fbf88c51d26a0d844cf1abb4dd
MingkaiChen <44886949+MingkaiChen@users.noreply.github.com>
2023-12-03 23:46:05 -0500

    Update file functions.

--- functions/file.ts
+++ functions/file.ts
@@ -16,7 +16,7 @@ export const onRequest: PagesFunction<Env> = async (context) => {
             var object_id = url.searchParams.get('object_id')
             var key = `${user_id}/${object_id}`;
             await context.env.ZAUNMAP_BUCKET.put(key, context.request.body);
-            return new Response(`Put ${key} successfully!`);
+            return new Response(`Put ${key} successfully!`, {status: 200, headers: {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'}});
         case 'GET':
             var object_id = url.searchParams.get('object_id')
             var key = `${user_id}/${object_id}`;
@@ -29,6 +29,7 @@ export const onRequest: PagesFunction<Env> = async (context) => {
             const headers = new Headers();
             object.writeHttpMetadata(headers);
             headers.set('etag', object.httpEtag);
+            headers.set('Access-Control-Allow-Origin', '*');
         
             return new Response(object.body, {
                 headers,
@@ -38,12 +39,20 @@ export const onRequest: PagesFunction<Env> = async (context) => {
             var key = `${user_id}/${object_id}`;
             await context.env.ZAUNMAP_BUCKET.delete(key);
             return new Response('Deleted!');
+        case 'OPTIONS':
+            return new Response(null, {
+                headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'PUT, GET, DELETE, POST, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                },
+            });
     
         default:
             return new Response('Method Not Allowed', {
                 status: 405,
                 headers: {
-                Allow: 'PUT, GET, DELETE, POST',
+                Allow: 'PUT, GET, DELETE, POST, OPTIONS',
                 },
             });
     }
