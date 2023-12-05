70fb72519f8cf164c7e775a948e5ce948d2adf79
Mingkai Chen <44886949+MingkaiChen@users.noreply.github.com>
2023-12-04 22:01:35 -0500

    Update file.ts

--- functions/file.ts
+++ functions/file.ts
@@ -11,7 +11,7 @@ export const onRequest: PagesFunction<Env> = async (context) => {
             var object_id = crypto.randomUUID()
             var key = `${user_id}/${object_id}`;
             await context.env.ZAUNMAP_BUCKET.put(key, context.request.body);
-            return new Response(JSON.stringify({'status': true, 'object_id': object_id}), {status: 201, headers: {'Content-Type': 'application/json'}});
+            return new Response(JSON.stringify({'status': true, 'object_id': object_id}), {status: 201, headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}});
         case 'PUT':
             var object_id = url.searchParams.get('object_id')
             var key = `${user_id}/${object_id}`;
