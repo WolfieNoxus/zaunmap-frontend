945ac9e9957dddffd284f287425d153a39f5a501
Mingkai Chen <44886949+MingkaiChen@users.noreply.github.com>
2023-12-05 13:22:04 -0500

    Update file.ts

--- functions/file.ts
+++ functions/file.ts
@@ -10,12 +10,12 @@ export const onRequest: PagesFunction<Env> = async (context) => {
         case 'POST':
             var object_id = crypto.randomUUID()
             var key = `${user_id}/${object_id}`;
-            await context.env.ZAUNMAP_BUCKET.put(key, context.request.body);
+            await context.env.ZAUNMAP_BUCKET.put(key, context.request.body, {httpMetadata: {contentType: context.request.headers.get('Content-Type')}});
             return new Response(JSON.stringify({'status': true, 'object_id': object_id}), {status: 201, headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}});
         case 'PUT':
             var object_id = url.searchParams.get('object_id')
             var key = `${user_id}/${object_id}`;
-            await context.env.ZAUNMAP_BUCKET.put(key, context.request.body);
+            await context.env.ZAUNMAP_BUCKET.put(key, context.request.body, {httpMetadata: {contentType: context.request.headers.get('Content-Type')}});
             return new Response(`Put ${key} successfully!`, {status: 200, headers: {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'}});
         case 'GET':
             var object_id = url.searchParams.get('object_id')
