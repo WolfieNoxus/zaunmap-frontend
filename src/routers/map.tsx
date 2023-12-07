25e99d23efb832f4f69d0cc46bde486e6bfd0034
MingkaiChen <44886949+MingkaiChen@users.noreply.github.com>
2023-12-07 12:41:09 -0500

    Adopt AdminPortal to New API.

--- src/routers/map.tsx
+++ src/routers/map.tsx
@@ -136,8 +136,8 @@ function Map() {
 
   if (loading) {
     return (
-      <div>
-        <h1>Loading...</h1>
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
       </div>
     );
   }
