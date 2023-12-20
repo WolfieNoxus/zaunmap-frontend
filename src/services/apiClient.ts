27b41253c8b3d385a9ee382745a8349e68be5dea
0xuana <lingxuan.fang@stonybrook.edu>
2023-12-20 13:04:19 -0500

    delete jwt update

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -2,30 +2,30 @@ import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
 // import { useEffect, useState } from "react";
 
-// export default axios.create({
-//   baseURL: "https://zaunmap-6b1455b08c9b.herokuapp.com/api",
-//   // headers: { Authorization: "Bearer " + user?.token },
+export default axios.create({
+  baseURL: "https://zaunmap-6b1455b08c9b.herokuapp.com/api",
+  // headers: { Authorization: "Bearer " + user?.token },
+});
+
+// const apiBaseUrl = "https://zaunmap-6b1455b08c9b.herokuapp.com/api";
+
+// const axiosInstance = axios.create({
+//   baseURL: apiBaseUrl,
 // });
 
-const apiBaseUrl = "https://zaunmap-6b1455b08c9b.herokuapp.com/api";
+// axiosInstance.interceptors.request.use(async (config) => {
+//   const { isAuthenticated, getAccessTokenSilently } = useAuth0();
 
-const axiosInstance = axios.create({
-  baseURL: apiBaseUrl,
-});
+//   if (isAuthenticated) {
+//     try {
+//       const accessToken = await getAccessTokenSilently();
+//       config.headers.Authorization = `Bearer ${accessToken}`;
+//     } catch (error) {
+//       console.error("Error getting access token:", error);
+//     }
+//   }
 
-axiosInstance.interceptors.request.use(async (config) => {
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+//   return config;
+// });
 
-  if (isAuthenticated) {
-    try {
-      const accessToken = await getAccessTokenSilently();
-      config.headers.Authorization = `Bearer ${accessToken}`;
-    } catch (error) {
-      console.error("Error getting access token:", error);
-    }
-  }
-
-  return config;
-});
-
-export default axiosInstance;
+// export default axiosInstance;
